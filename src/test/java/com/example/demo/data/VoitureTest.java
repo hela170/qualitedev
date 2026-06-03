6e843c6a248cf7bcb1ee2a2db6204fcd2e213f68
root <root@Hela.localdomain>
2026-06-03 10:50:35 +0200

    Ajout du test unitaire creerVoiture pour le TP1

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -2,7 +2,7 @@ package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
+import org.springframework.util.Assert;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +11,13 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+        Voiture voiture = new Voiture ("Ferrari", 5000); [cite: 43]
+        
+        Assert.isTrue(voiture.getMarque().equals("Ferrari"), "Doit être Ferrari"); [cite: 44]
+        Assert.isTrue(voiture.getPrix() == 5000, "Doit être 5000"); [cite: 45]
+        
+        // La clef primaire n'a pas été générée [cite: 46]
+        Assert.isTrue(voiture.getId() == 0, "Doit être 0"); [cite: 47]
     }
 
 }
