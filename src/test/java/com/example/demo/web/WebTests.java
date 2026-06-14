2168966acb295d64d981180e6b83cc061f6b67f6
root <root@Hela.localdomain>
2026-06-14 16:17:17 +0200

    Ajout tests MockMvc

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -1,6 +1,5 @@
 package com.example.demo.web;
 
-import com.example.demo.data.Voiture;
 import com.example.demo.service.Echantillon;
 import com.example.demo.service.StatistiqueImpl;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -27,4 +25,27 @@ class WebTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    void testGetStatistique() throws Exception {
+
+        when(statistiqueImpl.prixMoyen())
+                .thenReturn(new Echantillon(2, 15000));
+
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(2))
+                .andExpect(jsonPath("$.prixMoyen").value(15000));
+    }
+
+    @Test
+    void testPostVoiture() throws Exception {
+
+        mockMvc.perform(post("/voiture")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"marque\":\"f\",\"prix\":100}"))
+                .andExpect(status().isOk());
+
+        verify(statistiqueImpl, times(1))
+                .ajouter(any());
+    }
 }
